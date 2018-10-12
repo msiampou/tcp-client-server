fc5cdd45548e358dfbf112432003ab474e01041d
Maria Despoina Siampou <siampoumaria@gmail.com>
2018-10-12 12:17:39 +0300

    Update thread.c

--- WebCrawler/thread.c
+++ WebCrawler/thread.c
@@ -18,11 +18,10 @@ void* thr_job(void* p){
     while(pool->finished == 0){
 
         pthread_mutex_lock(&(pool->lock));
-        printf("\n[%lu] mutex locked\n",pthread_self());
+      
         while (pool->JobQueue->count<=0){
 
             pool->JobQueue->done++;
-            printf("[%lu] Increased done: %d\n", pthread_self(),pool->JobQueue->done);
 
             if((pool->JobQueue->done>=pool->num_threads) && (i!=0)){
                 pool->finished = 1;
@@ -32,15 +31,13 @@ void* thr_job(void* p){
 
             if(pthread_cond_wait(&(pool->cond), &(pool->lock)) !=0)
                 printf("error\n");
-            printf("[%lu] Got out of wait\n",pthread_self());
+           
             if(pool->finished == 1)
                 break;
-
-            //pool->JobQueue->done--;
+            
         }
         if(pool->finished == 1){
             pthread_mutex_unlock(&(pool->lock));
-            printf("\nmutex unlocked\n");
             break;
         }
 
@@ -53,21 +50,17 @@ void* thr_job(void* p){
         if(task!=NULL){
             char* resp = SocketConnection(pool->socket,task->url,&(pool->statistics->bytes));
             FileCreate(pool, task->url, resp);
-            printf("after connect\n");
             pthread_mutex_unlock(&(pool->lock));
-            printf("[%lu] mutex unlocked\n",pthread_self());
             char* parser = task->url;
             FileManagment(pool,resp, task->url);
             free(parser);
             free(task);
             close(pool->socket->sockfd);
         }
-        else{
+        else
             pthread_mutex_unlock(&(pool->lock));
-            printf("[%lu] mutex unlocked\n",pthread_self());
-        }
+        
         pthread_cond_broadcast(&(pool->cond));
-
     }
     printf("Crawling Process Ended\n");
     pthread_exit(NULL);
